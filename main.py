2829d1aa8679856cd80e3cfe0bd583ad077c48c4
genipsy <genipsy@gmail.com>
2025-12-19 20:47:59 +0900

    사이트 변경

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ service = Service(executable_path=os.getenv('CHROME_DRIVER_PATH'))
 driver = webdriver.Chrome(service=service)
 
 try:
-  driver.get('https://www.naver.com')
+  driver.get('https://www.google.com')
   time.sleep(3)
-
+  print(driver.title) # 페이지 타이틀 출력
 finally:
   driver.quit()
