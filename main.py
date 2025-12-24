2bd82660a82c7634e80a7508bd6c557c53b52e7f
genipsy <genipsy@gmail.com>
2025-12-24 21:17:46 +0900

    1. 구글 크롤링 테스트
    2. keyword.txt를 이용한 키워드목록 추출
    
    3. readme.md에 내용 추가
    
    2025-12-24 메리크리스마스

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import os
 import time
 
 service = Service(executable_path=os.getenv('CHROME_DRIVER_PATH'))
 
-driver = webdriver.Chrome(service=service)
 
 try:
-  driver.get('https://www.google.com')
+  options = webdriver.ChromeOptions()
+  
+  # 로봇이 아닙니다 방지
+  options.add_argument("--disable-blink-features=AutomationControlled")
+  driver = webdriver.Chrome(service=service, options=options)
+  
+  # 키워드 파일을 읽는다
+  with open('keyword.txt', 'r') as file:
+    keywords = file.readlines()
+    for keyword in keywords:
+      keyword = keyword.strip() # 키워드 공백제거
+      print(keyword)
+      driver.get('https://www.google.com')
+      driver.implicitly_wait(1)
+      print(driver.title) # 페이지 타이틀 출력
+      elem = driver.find_element(By.NAME, "q") # 검색창 찾기
+      elem.clear() # 검색창 초기화
+      elem.send_keys(keyword) # 검색창에 'Python' 입력
+      elem.send_keys(Keys.RETURN) # 검색창에 입력한 내용 전송
+      time.sleep(1)
+
   time.sleep(3)
-  print(driver.title) # 페이지 타이틀 출력
+  driver.close()
 finally:
   driver.quit()
