98e80097505f08c6f5bc478de68e7a5cdd2d39c1
genipsy <genipsy@gmail.com>
2025-12-31 19:03:22 +0900

    스크래핑 모듈 컨택스트 매니저로 이동

--- main.py
+++ main.py
@@ -1,36 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import os
-import time
+from web_scraper import ChromiumScraper
 
-service = Service(executable_path=os.getenv('CHROME_DRIVER_PATH'))
+with open('keyword.txt', 'r') as file:
+  keywords = file.readlines()
 
-
-try:
-  options = webdriver.ChromeOptions()
-  
-  # 로봇이 아닙니다 방지
-  options.add_argument("--disable-blink-features=AutomationControlled")
-  driver = webdriver.Chrome(service=service, options=options)
-  
-  # 키워드 파일을 읽는다
-  with open('keyword.txt', 'r') as file:
-    keywords = file.readlines()
-    for keyword in keywords:
-      keyword = keyword.strip() # 키워드 공백제거
-      print(keyword)
-      driver.get('https://www.google.com')
-      driver.implicitly_wait(1)
-      print(driver.title) # 페이지 타이틀 출력
-      elem = driver.find_element(By.NAME, "q") # 검색창 찾기
-      elem.clear() # 검색창 초기화
-      elem.send_keys(keyword) # 검색창에 'Python' 입력
-      elem.send_keys(Keys.RETURN) # 검색창에 입력한 내용 전송
-      time.sleep(1)
-
-  time.sleep(3)
-  driver.close()
-finally:
-  driver.quit()
+with ChromiumScraper('google') as s:
+  for keyword in keywords:
+    s.search(keyword.strip())
+  print(s.driver.page_source)
---
+++ web_scraper.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+
+# 구글 검색 정보
+google_search = {
+  'url': 'https://www.google.com',
+  'search_box': 'textarea[name="q"]',
+  'search_button': 'input[type="submit"][name="btnK"]',
+  'search_results': 'div.MjjYud > div.A6K0A'
+                 }
+
+# 검색 타입(일단 구글만 먼저)
+srch_dict = {'google': google_search}
+
+class ChromiumScraper:
+
+  # 생성자
+  def __init__(self, srch_type):
+     # 검색 타입 체크
+    if srch_type not in srch_dict:
+      raise ValueError(f"Invalid search type: {srch_type}")
+    self.srch_info = srch_dict.get(srch_type)
+  
+  # 컨텍스트 매니저 시작
+  def __enter__(self):
+    print(f"ChromiumScraper __enter__: {self.srch_info}")
+    try:
+      service = Service(executable_path=os.getenv('CHROME_DRIVER_PATH'))
+      options = webdriver.ChromeOptions()
+      # 로봇이 아닙니다 방지
+      options.add_argument("--disable-blink-features=AutomationControlled")
+      self.driver = webdriver.Chrome(service=service, options=options)
+      return self
+    
+    except Exception as e:
+      print(f"Error: {e}")
+      return None
+
+  # 컨텍스트 매니저 종료
+  def __exit__(self, exc_type, exc_value, traceback):
+      print("ChromiumScraper __exit__")
+      self.driver.quit()
+  
+  # 검색 시작
+  def search(self, keyword):
+    print(f"ChromiumScraper search: {keyword}")
+    print(f"ChromiumScraper search: {self.srch_info}")
+    self.driver.get(self.srch_info['url'])
+    self.driver.implicitly_wait(1)
+    elem = self.driver.find_element(By.CSS_SELECTOR, self.srch_info['search_box'])
+    elem.clear() # 검색창 초기화
+    elem.send_keys(keyword) # 검색창에 'Python' 입력
+    elem.send_keys(Keys.RETURN) # 검색창에 입력한 내용 전송
+    time.sleep(1)
+    return self.driver.page_source
+
